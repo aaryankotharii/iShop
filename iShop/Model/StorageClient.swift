801a2717c3f59176fe066bf702939cbf2bf7de36
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-12 01:06:53 +0530

    StorageClient added

---
+++ iShop/Model/StorageClient.swift
@@ -0,0 +1,14 @@
+//
+//  StorageClient.swift
+//  iShop
+//
+//  Created by Aaryan Kothari on 12/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import FirebaseStorage
+
+class StorageClient{
+
+
+}
