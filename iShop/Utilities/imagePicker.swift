becb1b9744b572b1991d7b3ab76794a52fb17179
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-11 23:52:39 +0530

    imagePickerAlert function added

--- iShop/Utilities/imagePicker.swift
+++ iShop/Utilities/imagePicker.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController: UIImagePickerControllerDelegate{
+    func imagePickerAlert(_ imageView: UIImageView){
+
+        //MARK: ImagePicker ActionSheet
+        let alertController = UIAlertController.init(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        //MARK: Actions [Delete + PhotoLibrary + Camera + Cancel]
+        let deleteAction = UIAlertAction(title: "Delete Photo", style: .destructive, handler: handleDeletePhoto(action:))
+        let photoLibraryAction = UIAlertAction.init(title: "Choose Photo", style: .default, handler: handleChoosePhoto(action:))
+        let cameraAction = UIAlertAction.init(title: "Take photo", style: .default, handler: handleCameraTapped(action:))
+        let cancelAction = UIAlertAction.init(title: "Cancel", style: .cancel, handler: handleCancel(action:))
+    
+        //MARK: Add Actions
+//        if imageView.image != self.defaultImage{
+//            alertController.addAction(deleteAction)
+//        }
+              alertController.addAction(photoLibraryAction)
+              alertController.addAction(cameraAction)
+              alertController.addAction(cancelAction)
+
+            self.present(alertController, animated: true)
+    }
+    
+    func handleDeletePhoto(action: UIAlertAction){
+        
+    }
+    
+    func handleChoosePhoto(action: UIAlertAction){
+           
+       }
+    
+    func handleCameraTapped(action: UIAlertAction){
+           
+       }
+    
+    func handleCancel(action: UIAlertAction){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+}
