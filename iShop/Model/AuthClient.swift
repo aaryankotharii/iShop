cb532c18ac4eb92201f4e0028b98a353fd677465
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-11 15:09:37 +0530

    GoogleService property list added

--- iShop/Model/AuthClient.swift
+++ iShop/Model/AuthClient.swift
@@ -14,6 +14,7 @@ class AuthClient{
     class func Login(email: String, password: String, completion: @escaping (Bool, Error?) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
             if let error = error{
+                print(AuthClient.handleError(error))
                 completion(false,error)
                 return
             }
@@ -29,5 +30,17 @@ class AuthClient{
             }
             completion(true,nil)
         }
+    }
+    
+    class func handleError(_ error : Error) -> String{
+        if let errorCode = AuthErrorCode(rawValue: error._code){
+            switch errorCode {
+            case .emailAlreadyInUse:
+                return "already in use"
+            default:
+                print("")
+            }
+        }
+        return "yo"
     }
 }
