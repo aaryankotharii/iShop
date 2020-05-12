34114da53caf7c8c8581df4287d8aa547ec0531c
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-12 14:36:12 +0530

    hideKeyboardWhenTappedAround func added

--- iShop/Utilities/Keyboard+Hide.swift
+++ iShop/Utilities/Keyboard+Hide.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    // Function for tap gesture
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    // Calling dismiss selector actions
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
