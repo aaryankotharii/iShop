3a74fcd5ece353a5f1f8235fcc1ddb22f1646d72
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-14 22:56:26 +0530

    Locked screen blur view added

---
+++ iShop/View/lockedScreen.swift
@@ -0,0 +1,21 @@
+//
+//  lockedScreen.swift
+//  iShop
+//
+//  Created by Aaryan Kothari on 14/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class lockedScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
