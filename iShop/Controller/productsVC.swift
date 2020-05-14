d8238494cc026bb060bedcedc48fe0872a94bce9
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-14 21:06:19 +0530

    tableView sample setup complete

--- iShop/Controller/productsVC.swift
+++ iShop/Controller/productsVC.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class productsVC: UIViewController {
+    /// TableView that displays products
+    @IBOutlet var productsTableView: UITableView!
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationController?.title = "Products"
-        self.title = "Products"
-        
-        self.navigationItem.title = "Products"
-
-        // Do any additional setup after loading the view.
+extension productsVC : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //MARK:- SAMPLE CELL
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.detailTextLabel?.text = "Product \(indexPath.row + 1)"
+        cell.backgroundView?.backgroundColor = #colorLiteral(red: 0.9985503554, green: 0.9494881034, blue: 0.5791102052, alpha: 1)
+        cell.contentView.backgroundColor = #colorLiteral(red: 0.9985503554, green: 0.9494881034, blue: 0.5791102052, alpha: 1)
+        return cell
+    }
 }
